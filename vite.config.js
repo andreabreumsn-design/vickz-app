71b961a3e158d25b84d57200583408784f2fc403
andreabreumsn-design <andreabreumsn@gmail.com>
2026-09-11 04:03:34 +0000

    preview: Vite estático para Cloudflare Pages

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,11 @@
-﻿import { defineConfig } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-import { cloudflare } from "@cloudflare/vite-plugin";
-
 export default defineConfig({
-  plugins: [react(), cloudflare()],
+  plugins: [react()],
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    minify: false,
-    sourcemap: true
+    sourcemap: false
   }
 })
